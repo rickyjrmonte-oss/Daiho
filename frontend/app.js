6b1862005696f9bcee2e5794b2379a9e46745c13
rickyjrmonte-oss <rickyjrmonte@gmail.com>
2026-02-13 23:48:57 +0800

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const API_BASE = "https://daiho-backend-2.onrender.com";
+const API_BASE = "https://daiho-backend-3.onrender.com";
 
 const saleBody = document.getElementById("saleBody");
 const grossSaleEl = document.getElementById("grossSale");
@@ -254,6 +254,7 @@ btnDownload.onclick = async () => {
 // --- Init ---
 
 loadForDate(currentDate);
+
 
 
 
